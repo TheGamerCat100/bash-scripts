94195b9ad3bb8c2d50b2606f12244615cf4197fc
TheGamerCat100 <40955862+TheGamerCat100@users.noreply.github.com>
2020-03-13 17:20:30 +0200

    Add files via upload

--- convert.sh
+++ convert.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd ~/Videos
-read -p "Please point to the file you want to convert (ends in .MP4) :" FILE1
-read -p "Please specifiy the final name (ends in .MOV) :" FILE2
-ffmpeg -i "$FILE1" -c:v mpeg4 -q:v 5 -c:a pcm_s16le "$FILE2"
+FILE1=${1?Error: Please point to the file in /home/Videos you want to convert (./convert.sh [FILENAME])}
+espeak "converting $FILE1"
+ffmpeg -i "$FILE1" -c:v mpeg4 -q:v 5 -c:a pcm_s16le "$FILE1_converted_to.mov"
+espeak "done!"
+espeak "may the lord be with you!"
